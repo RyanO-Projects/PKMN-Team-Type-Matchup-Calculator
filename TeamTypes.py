877f6e5c16c886a96d5425d3371ef48f4a53d5c2
Ryan O <ryan.opitz@mycc.caspercollege.edu>
2024-01-09 20:32:32 -0700

    PokeAPI Integration
    Added functionality to call PokeAPI to gather information for pokemon types and relevant damage information. Added clarification that this is currently only for defensive strength/weakness.

--- TeamTypes.py
+++ TeamTypes.py
@@ -1,102 +1,60 @@
-#   TYPE = ([STRONG AGAINST], [WEAK AGAINST], [NO EFFECT])
+import requests
 
-NORMAL = ([],
-          ['rock','steel'],
-          ['ghost'])
+# Fetch type data from PokeAPI and store it in a dictionary
+def fetch_type_data():
+    OK = 200
+    type_data = {}
 
-FIRE = (['grass','ice','bug','steel'],
-        ['fire','water','rock','dragon'],
-        [])
+    response = requests.get("https://pokeapi.co/api/v2/type/")
+    if response.status_code == OK:
+        types = response.json()["results"]
+        for type_info in types:
+            type_name = type_info["name"]
+            type_response = requests.get(type_info["url"])
+            if type_response.status_code == OK:
+                damage_relations = type_response.json()["damage_relations"]
+                super_effective = [t["name"] for t in damage_relations["double_damage_from"]]
+                not_very_effective = [t["name"] for t in damage_relations["half_damage_from"]]
+                no_effect = [t["name"] for t in damage_relations["no_damage_from"]]
+                type_data[type_name] = {
+                    "super_effective" : super_effective,
+                    "not_very_effective" : not_very_effective,
+                    "no_effect" : no_effect
+                }
+    return type_data
 
-WATER = (['fire','ground','rock'],
-         ['water','grass','dragon'],
-         [])
-
-ELECTRIC = (['water','flying'],
-            ['electric','grass','dragon'],
-            ['ground'])
-
-GRASS = (['water','ground','rock'],
-         ['fire','grass','poison','flying','bug','dragon','steel'],
-         [])
-
-ICE = (['grass','ground','flying','dragon'],
-       ['fire','water','ice','steel'],
-       [])
-
-FIGHTING = (['normal','ice','rock','dark','steel'],
-            ['poison','flying','psychic','bug','fairy'],
-            ['ghost'])
-
-POISON = (['grass','fairy'], 
-          ['poison','ground','rock','ghost'],
-          ['steel'])
-
-GROUND = (['fire','electric','poison','rock','steel'],
-          ['grass','bug'],
-          ['flying'])
-
-FLYING = (['grass','fighting','bug'],
-          ['electric','rock','steel'],
-          [])
-
-PSYCHIC = (['fighting','poison'],
-           ['psychic','steel'],
-           ['dark'])
-
-BUG = (['grass','psychic','dark'],
-       ['fire','fighting','poison','flying','ghost','steel','fairy'],
-       [])
-
-ROCK = (['fire','ice','flying','bug'],
-        ['fighting','ground','steel'],
-        [])
-
-GHOST = (['psychic','ghost'],
-         ['dark'],
-         ['normal'])
-
-DRAGON = (['dragon'],
-          ['steel'],
-          ['fairy'])
-
-DARK = (['psychic','ghost'],
-        ['fighting','dark','fairy'],
-        [])
-
-STEEL = (['ice','rock','fairy'],
-         ['fire','water','electric','steel'],
-         [])
-
-FAIRY = (['fighting','dragon','dark'],
-         ['fire','poison','dark'],
-         [])
-
-
-def tally(pokemon, type_dict):
-    for type in pokemon:
-        for type_key in type_dict:
-            effectiveness = globals()[type_key.upper()]
-            if type in effectiveness[0]:  #checking strengths
-                type_dict[type_key] -= 1
-            if type in effectiveness[1]:  #checking weaknesses
-                type_dict[type_key] += 1
-            if type in effectiveness[2]:
-                type_dict[type_key] += 2
+# Determine defensive strength/weakness to each type for each PKMN in team
+def tally(pokemon, type_dict, type_data):
+    for type_name in pokemon:
+        type_info = type_data.get(type_name)
+        if type_info:
+            for key in type_dict:
+                if key in type_info["super_effective"]:  #check weakness
+                    type_dict[key] -= 1
+                if key in type_info["not_very_effective"]:  #check resist
+                    type_dict[key] += 1
+                if key in type_info["no_effect"]:   #check no-effect
+                    type_dict[key] += 2
     
     return type_dict
 
 def main():
-    team = (['fire'],['psychic','flying'], ['flying','poison'], ['water','flying'], ['fighting'])
+    type_data = fetch_type_data()
+
     type_dict = {
         'normal' : 0, 'fire' : 0, 'water' : 0, 'electric' : 0,
         'grass' : 0, 'ice' : 0, 'fighting' : 0, 'poison' : 0,
-        'ground' : 0, 'flying' : 0, 'psychic' : 0, 'bug' : 0,
-        'rock' : 0, 'ghost' : 0, 'dragon' : 0, 'steel' : 0, 'fairy' : 0
+        'ground' : 0, 'flying' : 0, 'psychic' : 0, 'bug' : 0, 'rock' : 0,
+        'ghost' : 0, 'dark' : 0, 'dragon' : 0, 'steel' : 0, 'fairy' : 0
         }
-    for pokemon in team:
-        tally(pokemon, type_dict)
+    
+    team = (['water', 'flying'],)
+    #team = (['fire'],['psychic','flying'], ['flying','poison'], ['water','flying'], ['fighting'])
 
+    for pokemon in team:
+        tally(pokemon, type_dict, type_data)
+
+    #print(type_data)
     print(type_dict)
 
 main()
